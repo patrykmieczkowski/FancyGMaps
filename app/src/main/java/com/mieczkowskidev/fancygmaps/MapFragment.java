284178979ce7bb0335c758041f164b3dd07cc8ad
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2015-10-18 12:43:24 +0200

    set listeners, define google api client, fix manifest

--- app/src/main/java/com/mieczkowskidev/fancygmaps/MapFragment.java
+++ app/src/main/java/com/mieczkowskidev/fancygmaps/MapFragment.java
@@ -1,5 +1,6 @@
 package com.mieczkowskidev.fancygmaps;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,30 +8,54 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 
 
-public class MapFragment extends com.google.android.gms.maps.MapFragment implements
+public class MapFragment extends SupportMapFragment implements
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
         GoogleMap.OnInfoWindowClickListener, GoogleMap.OnMapLongClickListener,
         GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener {
+//
+//    public static MapFragment newInstance() {
+//        MapFragment fragment = new MapFragment();
+//        return fragment;
+//    }
+//
+//    public MapFragment() {
+//        // Required empty public constructor
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_map, container, false);
+//    }
 
-    public static MapFragment newInstance() {
-        MapFragment fragment = new MapFragment();
-        return fragment;
-    }
+    private GoogleApiClient googleApiClient;
+    private Location currentLocation;
 
-    public MapFragment() {
-        // Required empty public constructor
-    }
+    private final int[] MAP_TYPES = {GoogleMap.MAP_TYPE_HYBRID,
+            GoogleMap.MAP_TYPE_SATELLITE, GoogleMap.MAP_TYPE_TERRAIN,
+            GoogleMap.MAP_TYPE_NORMAL, GoogleMap.MAP_TYPE_NONE};
+
+    private int curMapTypeIndex = 0;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        googleApiClient = new GoogleApiClient.Builder(getActivity())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        initListeners();
     }
 
     @Override
@@ -67,4 +92,14 @@ public class MapFragment extends com.google.android.gms.maps.MapFragment impleme
     public boolean onMarkerClick(Marker marker) {
         return false;
     }
+
+    private void initListeners(){
+        getMap().setOnMarkerClickListener(this);
+        getMap().setOnMapLongClickListener(this);
+        getMap().setOnInfoWindowClickListener(this);
+        getMap().setOnMapClickListener(this);
+
+    }
+
+
 }
